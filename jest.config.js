38e33c5634a727ce9a0eb558554418c67353becc
William Fernandes <wfoj.code@gmail.com>
2021-08-06 00:14:18 -0300

    add
    add husky, lintsaged, standard and jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  coverageDirectory: "coverage",
+
+  testEnvironment:"node",
+
+  clearMocks: true,
+
+  coverageProvider: "v8"
+
+};
