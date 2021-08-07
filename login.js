360d04d6cfd3d9d355dcb720655a7862a296e1f8
William Fernandes <wfoj.code@gmail.com>
2021-08-07 19:35:03 -0300

    design patterns, Domain, Presentation, Infra, Main
    Nessa aula foi explicar como desacoplar os componentes de frameworks como express e mongoose.

--- login.js
+++ login.js
@@ -1,26 +1,56 @@
 const express = require('express')
 const router = express.Router()
+
+module.exports = () => {
+  const router = new SignUpRouter()
+  router.post('/signup', ExpressRouterAdapter.adapt(router))
+
+}
+
+class ExpressRouterAdapter {
+  static adapt (router) {
+    return async (req, res) => {
+      const httpRequest = {
+        body: req.body
+      }
+      const httpResponse = await router.route(httpRequest)
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    }
+  }
+}
+// Pesentation
+// signup-router
+
+class SignUpRouter {
+  async route (httpRequest) {
+  const { email, password, repeatPassword } = httpRequest.body
+  const user = new SignUpUseCase().signUp( email, password, repeatPassword)
+    return {
+    statusCode:200,
+    body: user
+    }
+}
+}
+// Domain
+// signup-usecase
+
+class SignUpUseCase {
+  async signUp ( email, password, repeatPassword) {
+    if (password === repeatPassword) {
+      new AddAccountRepository().add(email, password)
+    }
+  }
+}
+
+// infra
+// add-account-repo
+
 const mongoose = require('mongoose')
 const AccountModel = mongoose.model('Account')
 
-module.exports = () => {
-  router.post('/signup', SignUpRouter().route)
-
-}
-
-class SignUpRouter {
-  async route (req, res) {
-  const { email, password, repeatPassword } = req.body
-  new SignUpUseCase().signUp( email, password, repeatPassword)
-res.status(400).json({ error: 'password must be equal to repeatPassword' })
-}
-}
-
-class SignUpUseCase {
-  async signUp ( email, password,repeatPassword {
-    if (password === repeatPassword) {
+class AddAccountRepository {
+  async add ( email, password, repeatPassword) {
       const user = await AccountModel.create({email, password })
       return user
     }
-  }
-} 
+}
