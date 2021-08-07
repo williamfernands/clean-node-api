814478bddabd1ec85adea1da150eb212a724f353
William Fernandes <wfoj.code@gmail.com>
2021-08-06 22:33:51 -0300

    test Architecture

---
+++ login.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+
+module.exports = () => {
+  router.post('/signup', SignUpRouter().route)
+
+}
+
+class SignUpRouter {
+  async route (req, res) {
+  const { email, password, repeatPassword } = req.body
+if (password === repeatPassword) {
+  const user = await AccountModel.create({email, password })
+  return res.json(user)
+}
+res.status(400).json({ error: 'password must be equal to repeatPassword' })
+}
+}
