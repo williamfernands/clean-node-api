a28fddd010dabb28c9cc57a88bd31ec0fa55cdb9
William Fernandes <wfoj.code@gmail.com>
2021-08-06 23:06:43 -0300

    config js

--- login.js
+++ login.js
@@ -11,10 +11,16 @@ module.exports = () => {
 class SignUpRouter {
   async route (req, res) {
   const { email, password, repeatPassword } = req.body
-if (password === repeatPassword) {
-  const user = await AccountModel.create({email, password })
-  return res.json(user)
-}
+  new SignUpUseCase().signUp( email, password, repeatPassword)
 res.status(400).json({ error: 'password must be equal to repeatPassword' })
 }
 }
+
+class SignUpUseCase {
+  async signUp ( email, password,repeatPassword {
+    if (password === repeatPassword) {
+      const user = await AccountModel.create({email, password })
+      return user
+    }
+  }
+} 
